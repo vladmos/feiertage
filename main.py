41b179b7ba85d3f90349f2fd2f10e1e99d2587bc
Vladimir Moskva <vladmos@gmail.com>
2024-11-03 20:20:35 +0100

    Set up a redirect

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from holidays import views
 app = flask.Flask(__name__)
 app.url_map.strict_slashes = False
 
+@app.before_request
+def hostname_redirect():
+    if (flask.request.host == 'german-holidays.fulc.ru' or
+            (not flask.request.is_secure and flask.request.host != 'localhost:8080')):
+        return flask.redirect('https://feiertage.vladmos.com' + flask.request.path)
+
+
 index = app.route(r'/')(views.index)
 calendar = app.route(r'/calendar')(views.calendar_view)
 warmup = app.route(r'/_ah/warmup')(views.warmup_view)
